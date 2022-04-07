23ad1245004957b9cfcc725fdc2965c07c1e90b4
lucasrafaeldesouza <faelsouza.rsg@hotmail.com>
2022-04-06 21:34:32 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Windows.Forms;
+//using Views;
+//using Controllers;
+
+namespace EncryptMe
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*Application.EnableVisualStyles();
+            Application.Run(new Login());*/
+            Console.WriteLine("Hellow");
+        }
+    }
+}
